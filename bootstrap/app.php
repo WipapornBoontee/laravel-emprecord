a7308eea1532b8a1528d28dd6a2e29b7db18256c
WipapornBoontee <148832773+WipapornBoontee@users.noreply.github.com>
2026-09-08 01:19:41 +0700

    แcc

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+    $middleware->trustProxies(at: '*'); 
         $middleware->validateCsrfTokens(except: [
             'api/github-webhook',
             'github-webhook',
+            
         ]);
 
         $middleware->alias([
