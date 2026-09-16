e169e7f8316b4acd6036920962274bfa2c2db7aa
ladawun11110 <151121260+ladawun11110@users.noreply.github.com>
2026-09-17 00:48:11 +0700

    Update salary_overtime.blade.php

--- resources/views/salary/salary_overtime.blade.php
+++ resources/views/salary/salary_overtime.blade.php
@@ -118,7 +118,9 @@
                         </div>
                         <div>
                             <h5 class="mb-0 fw-bold">สรุปค่าล่วงเวลา (OT)</h5>
-                            
+                             <span class="badge bg-primary-subtle text-primary border border-primary-subtle px-3 py-2 rounded-pill shadow-sm">
+                                    <i class="bi bi-cash-stack me-1"></i> เรทล่วงเวลา: {{ number_format($hourlyRate, 2) }} บาท/ชม.
+                                </span>
                         </div>
                     </div>
                     <div class="ot-month-badge">
@@ -135,9 +137,7 @@
                                     <i class="bi bi-person-fill" style="font-size: 2rem;"></i>
                                 </div>
                                 <h6 class="fw-bold text-dark mb-2">{{ Auth::user()->name }}</h6> -->
-                                <span class="badge bg-primary-subtle text-primary border border-primary-subtle px-3 py-2 rounded-pill shadow-sm">
-                                    <i class="bi bi-cash-stack me-1"></i> เรทล่วงเวลา: {{ number_format($hourlyRate, 2) }} บาท/ชม.
-                                </span>
+                               
                             </div>
                         </div>
                         <div class="col-md-6">
