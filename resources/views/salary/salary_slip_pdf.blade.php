e3b8e8b460686d53af40374a177ec017151993e6
ladawun11110 <151121260+ladawun11110@users.noreply.github.com>
2026-09-16 23:49:20 +0700

    Update salary_slip_pdf.blade.php

--- resources/views/salary/salary_slip_pdf.blade.php
+++ resources/views/salary/salary_slip_pdf.blade.php
@@ -134,7 +134,7 @@
                 เงินรับสุทธิ (Net Pay)
             </td>
             <td width="25%" class="font-bold text-right" style="font-size: 16px;">{{ number_format($netSalary, 2) }}</td>
-            <td width="50%" class="font-bold text-left" style="font-size: 14px; padding-left: 20px; color: #626262ff; font-style: italic;">({{ $netSalaryText }})</td>
+            <td width="50%" class="font-bold text-left" style="font-size: 14px; padding-center: 15px; color: #626262ff; font-style: italic;">({{ $netSalaryText }})</td>
         </tr>
     </table>
 
