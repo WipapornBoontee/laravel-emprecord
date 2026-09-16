335fac6fefe06fb9f107ead380c7bf600cfd6fa5
ladawun11110 <151121260+ladawun11110@users.noreply.github.com>
2026-09-16 16:39:35 +0700

    am

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -9,17 +9,18 @@ class SalaryController extends Controller
 {
     public function show()
     {
-        $user = \Illuminate\Support\Facades\Auth::user();
-        $salaryData = $user->salaryEmp;
+        // $user = \Illuminate\Support\Facades\Auth::user();
+        // $salaryData = $user->salaryEmp;
         
-        $baseSalary = $salaryData ? $salaryData->salary : 0;
-        $daysInMonth = \Carbon\Carbon::now()->daysInMonth;
+        // $baseSalary = $salaryData ? $salaryData->salary : 0;
+        // $daysInMonth = \Carbon\Carbon::now()->daysInMonth;
         
-        $dailyWage = 0;
-        if ($daysInMonth > 0 && $baseSalary > 0) {
-            $dailyWage = $baseSalary / $daysInMonth;
-        }
+        // $dailyWage = 0;
+        // if ($daysInMonth > 0 && $baseSalary > 0) {
+        //     $dailyWage = $baseSalary / $daysInMonth;
+        // }
 
-        return view('salary.salary_show', compact('baseSalary', 'daysInMonth', 'dailyWage'));
+        // return view('salary.salary_show', compact('baseSalary', 'daysInMonth', 'dailyWage'));
+        return view('salary.salary_show');
     }
 }
--- app/Models/SalaryEmp.php
+++ app/Models/SalaryEmp.php
@@ -11,7 +11,7 @@ class SalaryEmp extends Model
 
     protected $fillable = [
         'user_id',
-        'salary',
+        // 'salary',
     ];
 
     public function user()
