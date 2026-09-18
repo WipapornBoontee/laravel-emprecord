421eff32ea0c103639d266e83eb00793f766e569
ladawun11110 <151121260+ladawun11110@users.noreply.github.com>
2026-09-18 23:29:03 +0700

    Update header.blade.php

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -71,7 +71,7 @@
                             @if(Auth::user()?->isEmployee())
                                 <hr>
                                 <li>
-                                    <a class="dropdown-item py-2" href="{{ route('overtime.overtime_show', [Auth::user()->id] , false) }}">
+                                    <a class="dropdown-item py-2" href="{{ route('overtime.show', [Auth::user()->id] , false) }}">
                                         <i class="bi bi-wallet-fill me-2 text-success"></i> การทำ(OT)
                                     </a>
                                 </li>
